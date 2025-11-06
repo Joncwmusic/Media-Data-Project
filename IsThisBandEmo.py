7e435396421cba0f275d71c16828ff335ea5c1c7
Jon Whelpley <jon.chris.whelpley@gmail.com>
2025-11-06 15:13:46 -0500

    Creating New Media Data Project

--- IsThisBandEmo.py
+++ IsThisBandEmo.py
@@ -1,6 +1,27 @@
+from bs4 import BeautifulSoup
 import requests
 
 def check_is_emo(artist_name):
-    return None
+
+    resp = requests.get("http://isthisbandemo.com/?band={}".format(artist_name.replace(' ','+')))
+    # print(resp.text)
+
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+    # Find all links on the page
+    all_divs = soup.find_all('div')
+    for div in all_divs:
+        if div.get("class"):
+            if div.get("class")[0] == "results":
+                header_tag = div.find('h2')
+                if header_tag:
+                    emo_result = header_tag.text
+    if emo_result is None:
+        raise ValueError("emo_result is NULL")
+        return None
+    else:
+        return emo_result
 
 #### div class="results col-xs-12"
+
+print(check_is_emo("primus"))
