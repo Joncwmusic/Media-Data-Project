b72e209b7ec07997b435d9d83df7c9fe41dee752
Jon Whelpley <jon.chris.whelpley@gmail.com>
2025-11-17 12:23:29 -0500

    Updating tmdb ingestion functions

--- tmdb Ingestion.py
+++ tmdb Ingestion.py
@@ -17,7 +17,12 @@ tmdb_key = os.getenv("TMDB_API_KEY")
 #     resp = requests.get(token_url, headers=tmdb_headers)
 #     return resp.text
 
+
 def search_actor_name(actor_name):
+    '''
+    :param actor_name: actor name to query in api search
+    :return: json response with search results
+    '''
     actor_name_query = urllib.parse.quote(actor_name)
     api_url = "https://api.themoviedb.org/3/search/person?query={}&include_adult=false&language=en-US&page=1".format(actor_name_query)
     tmdb_header = {'Authorization': 'Bearer ' + tmdb_token}
@@ -26,6 +31,11 @@ def search_actor_name(actor_name):
 
 
 def get_actor_data(actor_id = None, actor_name = "Morgan Freeman"):
+    '''
+    :param actor_id: actor_id from tmdb supplied or otherwise searched for with actor_name query
+    :param actor_name: actor_name to invoke search_actor_name function if actor_id is not provided
+    :return: json response of actor data
+    '''
     if actor_id == None:
         json_search_resp = search_actor_name(actor_name)
         actor_id = json_search_resp["results"][0]["id"]
@@ -36,17 +46,60 @@ def get_actor_data(actor_id = None, actor_name = "Morgan Freeman"):
     print(actor_data_resp.text)
     return actor_data_resp.json()
 
-def get_movie_data(movie_name):
-    return None
 
-def get_most_popular_movies():
-    return None
+def search_movie_name(movie_name):
+    '''
+    :param movie_name: movie name query string
+    :return: json results from movie search
+    '''
+    movie_name_query = urllib.parse.quote(movie_name)
+    api_url = "https://api.themoviedb.org/3/search/movie?query={}&include_adult=false&language=en-US&page=1".format(movie_name_query)
+    tmdb_header = {'Authorization': 'Bearer ' + tmdb_token}
+    resp = requests.get(api_url, headers=tmdb_header)
+    return resp.json()
 
-def get_most_popular_tv_shows():
-    return None
 
-def get_most_popular_movie_people():
-    return None
+def get_movie_data(movie_id = None, movie_name = "The Shawshank redemption"):
+    if movie_id == None:
+        json_search_resp = search_actor_name(movie_name)
+        print(json_search_resp.text)
+        #movie_id = json_search_resp["results"][0]["id"]
+    actor_url = "https://api.themoviedb.org/3/movie/{}?language=en-US".format(movie_id)
+    tmdb_headers = {'Authorization': 'Bearer ' + tmdb_token}
+    actor_data_resp = requests.get(actor_url, headers= tmdb_headers)
+    return actor_data_resp.json()
+
+def get_trending_movies(week = True):
+    if week:
+        time_window = "week"
+    else:
+        time_window = "day"
+    api_url = "https://api.themoviedb.org/3/trending/movie/{}}?language=en-US".format(time_window)
+    tmdb_header = {'Authorization': 'Bearer ' + tmdb_token}
+    resp = requests.get(api_url, headers=tmdb_header)
+    return resp.json()
+
+
+def get_trending_tv_shows(week=True):
+    if week:
+        time_window = "week"
+    else:
+        time_window = "day"
+    api_url = "https://api.themoviedb.org/3/trending/movie/{}}?language=en-US".format(time_window)
+    tmdb_header = {'Authorization': 'Bearer ' + tmdb_token}
+    resp = requests.get(api_url, headers=tmdb_header)
+    return resp.json()
+
+
+def get_trending_movie_people(week=True):
+    if week:
+        time_window = "week"
+    else:
+        time_window = "day"
+    api_url = "https://api.themoviedb.org/3/trending/movie/{}}?language=en-US".format(time_window)
+    tmdb_header = {'Authorization': 'Bearer ' + tmdb_token}
+    resp = requests.get(api_url, headers=tmdb_header)
+    return resp.json()
 
 def get_most_popular(type = "movie"):
     '''
@@ -54,11 +107,8 @@ def get_most_popular(type = "movie"):
     :return: most popular items of that type from TMDB API
     '''
     if type == "movie":
-        get_most_popular_movies()
+        get_trending_movies()
     if type == "people":
-        get_most_popular_movie_people()
+        get_trending_movie_people()
     if type == "tv":
-        get_most_popular_tv_shows()
-
-
-print(get_actor_data(actor_id=None, actor_name="Chris Pratt"))
+        get_trending_tv_shows()
