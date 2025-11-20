e6e15387a76bacdf09939b55ad213ff7db24fcfd
Jon Whelpley <jon.chris.whelpley@gmail.com>
2025-11-20 15:49:59 -0500

    Added skeleton out streamlit dash

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+
 
 if __name__ == '__main__':
-    print("Hello World!")
+    st.title("Media Dashboard: Find data on your favorite artists, actors and movies!")
+
+    ##### Things to put on the dashbaord
+    # Search for artist function
+    # Graph stuff over time or one number stats
