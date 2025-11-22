9ee65be5a9de2201aeee883a1d39f8735405c5a4
Jon Whelpley <jon.chris.whelpley@gmail.com>
2025-11-22 11:34:52 -0500

    Added skeleton out streamlit dash

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import streamlit as st
 import pandas as pd
+import spotifyIngestion as si
+
+
+### Looks like streamlit has iframe and html componenets
+###
+
 
 
 if __name__ == '__main__':
     st.title("Media Dashboard: Find data on your favorite artists, actors and movies!")
 
+    tab1, tab2 = st.tabs(["Music", "Movies and TV"])
     ##### Things to put on the dashbaord
     # Search for artist function
-    # Graph stuff over time or one number stats
+
+    with tab1:
+        st.header("Music")
+        st.text("Artist: ")
+        artist_search_string = st.text_input("Search for an artist!")
+
+        # Graph stuff over time or one number stats
+        # # Number of monthly listeners (over time)
+        # # Number of followers (over time) resp_json["followers"]["total"]
+        # # Direct artist comparison like google trends (up to 3 artists)
+        # # Number of streams (across all tracks or per track)
+        # # Number of songs
+
+        # Analytics Visualizations
+        # # Patterns of growth for big artists
+        # #
+
+        # Search for Track/Album function
+        # # Skip Rate and Avg Listening Time
