6728f6ac2b46bf3f245745b914bfed380a07eea8
Jon Whelpley <jon.chris.whelpley@gmail.com>
2025-12-18 14:53:48 -0500

    json info to pandas df for artist info

--- ingestion/MostStreamedArtistList.py
+++ ingestion/MostStreamedArtistList.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 from ingestion import spotifyIngestion
 
 top_artist_url = "https://kworb.net/spotify/artists.html"
@@ -27,6 +28,25 @@ def create_pandas_record_mult(artist_id_list):
     artist_response = requests.get('https://api.spotify.com/v1/artists/{}'.format(artist_id_mult_string),
                                    headers=artist_headers)
     ## extract json components in a dict
+    artist_json = artist_response.json()
+    artist_dict = {"id":[], "Name":[], "Followers":[], "Popularity":[], "Genres":[], "Image_url":[]}
+
+    for artist in artist_json["artists"]:
+        artist_dict["id"].append(artist["id"])
+        artist_dict["followers"].append(artist["followers"]["total"])
+        artist_dict["Genres"].append(artist["genres"])
+        try:
+            artist_dict["Image_url"].append(artist["images"][0]["url"])
+        except:
+            artist_dict["Image_url"].append(None)
+        artist_dict["Name"].append(artist["name"])
+        artist_dict["Popularity"].append(artist["popularity"])
+
+        print(artist_dict)
+
+        return pd.DataFrame(artist_dict)
+        #compile data into dictionary to convert to pandas df
+
     artist_json_resp = artist_response.json()
 # Use that to call the endpoint (use multiple artist api call)
 
