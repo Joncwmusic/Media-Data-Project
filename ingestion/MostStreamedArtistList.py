4ae5ec3713f6ae1b60ad6991c2a52694a4fa1b83
Jon Whelpley <jon.chris.whelpley@gmail.com>
2025-12-22 15:27:22 -0500

    json info to pandas df for artist info

--- ingestion/MostStreamedArtistList.py
+++ ingestion/MostStreamedArtistList.py
@@ -19,8 +19,24 @@ def create_pandas_record(artist_id):
     artist_response = requests.get('https://api.spotify.com/v1/artists/{}'.format(artist_id),
                                    headers=artist_headers)
     artist_json_resp = artist_response.json()
+
+    # print(artist_json_resp)
+
     ## extract json components in a dict
-    return artist_response.json()
+    artist_dict = {"id": [], "Name": [], "Followers": [], "Popularity": [], "Genres": [], "Image_url": [],
+                   "Timestamp": []}
+
+    artist_dict["id"].append(artist_json_resp["id"])
+    artist_dict["Followers"].append(artist_json_resp["followers"]["total"])
+    artist_dict["Genres"].append(artist_json_resp["genres"])
+    try:
+        artist_dict["Image_url"].append(artist_json_resp["images"][0]["url"])
+    except:
+        artist_dict["Image_url"].append(None)
+    artist_dict["Name"].append(artist_json_resp["name"])
+    artist_dict["Popularity"].append(artist_json_resp["popularity"])
+    artist_dict["Timestamp"].append(dt.datetime.now())
+    return pd.DataFrame(artist_dict)
 
 def create_pandas_record_mult(artist_id_list):
     token = spotifyIngestion.get_spotify_token()
@@ -30,11 +46,12 @@ def create_pandas_record_mult(artist_id_list):
                                    headers=artist_headers)
     ## extract json components in a dict
     artist_json = artist_response.json()
-    print(artist_json)
-    artist_dict = {"id":[], "Name":[], "Followers":[], "Popularity":[], "Genres":[], "Image_url":[], "Timestamp":[]}
+    # print(artist_json)
+    artist_dict = {"id": [], "Name": [], "Followers": [], "Popularity": [], "Genres": [], "Image_url": [],
+                   "Timestamp": []}
 
     for artist in artist_json["artists"]:
-        print(artist)
+        # print(artist)
         artist_dict["id"].append(artist["id"])
         artist_dict["Followers"].append(artist["followers"]["total"])
         artist_dict["Genres"].append(artist["genres"])
@@ -46,7 +63,7 @@ def create_pandas_record_mult(artist_id_list):
         artist_dict["Popularity"].append(artist["popularity"])
         artist_dict["Timestamp"].append(dt.datetime.now())
 
-        print(artist_dict)
+        # print(artist_dict)
 
     return pd.DataFrame(artist_dict)
         #compile data into dictionary to convert to pandas df
@@ -61,7 +78,8 @@ def create_pandas_record_mult(artist_id_list):
 
 
 ######## Testing
-skrillex_id = get_artist_id("skrillex")
-origami_angel_id = get_artist_id("Origami Angel")
-print([skrillex_id, origami_angel_id])
-print(create_pandas_record_mult([skrillex_id, origami_angel_id]))
+# skrillex_id = get_artist_id("skrillex")
+# origami_angel_id = get_artist_id("Origami Angel")
+# print(create_pandas_record(skrillex_id))
+# print([skrillex_id, origami_angel_id])
+# print(create_pandas_record_mult([skrillex_id, origami_angel_id]))
